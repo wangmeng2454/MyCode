0c6c0256f6b2191558619388767e28249c04dbf9
wangmeng2454 <2454238486@qq.com>
2023-05-22 00:11:01 +0800

    12. 整数转罗马数字

--- src/main/java/IntToRoman12.java
+++ src/main/java/IntToRoman12.java
@@ -7,13 +7,23 @@ import org.slf4j.LoggerFactory;
 
 public class IntToRoman12 {
     public static void main(String[] args) {
-        LoggerFactory.getLogger(IntToRoman12.class).info(intToRoman(1994));
+        LoggerFactory.getLogger(IntToRoman12.class).info("1994 - {}", intToRoman(1994));
 
     }
 
     public static String intToRoman(int num) {
+        int index = 0;
+        int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder sb = new StringBuilder();
 
-        return null;
+        while (index < nums.length) {
+            while (num >= nums[index]) {
+                sb.append(romans[index]);
+                num -= nums[index];
+            }
+            index++;
+        }
+        return sb.toString();
     }
-
 }
