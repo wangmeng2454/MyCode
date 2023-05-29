2cc099787c42fbfcb6ba0fd77101376dc176b6f8
wangmeng2454 <2454238486@qq.com>
2023-05-29 23:44:06 +0800

    No35SearchInsert

--- src/main/java/No27RemoveElement.java
+++ src/main/java/No27RemoveElement.java
@@ -14,7 +14,8 @@ public class No27RemoveElement {
         Logger logger = LoggerFactory.getLogger(No27RemoveElement.class);
         int[] ints = {0, 1, 2, 2, 3, 0, 4, 2};
         logger.info("[0,1,2,2,3,0,4,2]移除数值为2后的长度为 - {}", removeElement(ints, 2));
-        logger.info("数组为 - {}", Arrays.toString(ints));
+        String str = Arrays.toString(ints);
+        logger.info("数组为 - {}", str);
     }
 
     public static int removeElement(int[] nums, int val) {
