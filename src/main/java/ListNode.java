e2d6fa14bad4840e24652c6670642b5921511abe
wangmeng2454 <2454238486@qq.com>
2023-06-04 21:47:37 +0800

    template

---
+++ src/main/java/ListNode.java
@@ -0,0 +1,22 @@
+/**
+ * @Description: TODO
+ * @CreateDate: 2023/6/4
+ */
+
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
